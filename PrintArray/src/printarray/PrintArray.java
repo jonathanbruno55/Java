24ac90bb6a1ff072d09dfef807d83614999c4687
Jonathan Bruno <52010584+jonathanbruno55@users.noreply.github.com>
2021-03-05 01:43:30 -0600

    Add files via upload

---
+++ PrintArray/src/printarray/PrintArray.java
@@ -0,0 +1,16 @@
+package printarray;
+
+import java.util.Arrays;
+
+public class PrintArray {
+
+    public static void main(String[] args) {
+       
+        int[] ints = new int[] {1, 2, 3, 4, 5};
+        String[] strings = new String[] {"Tom", "Mary", "Bill", "Hank"};
+    
+        System.out.println(Arrays.toString(ints));
+        System.out.println(Arrays.toString(strings));
+    
+  }
+}
