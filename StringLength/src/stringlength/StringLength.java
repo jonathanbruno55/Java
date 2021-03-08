c38b631018170795f9f2cfd6e8e1fcbf4eb4af49
Jonathan Bruno <52010584+jonathanbruno55@users.noreply.github.com>
2021-03-08 03:30:40 -0600

    Add files via upload

---
+++ StringLength/src/stringlength/StringLength.java
@@ -0,0 +1,13 @@
+package stringlength;
+
+public class StringLength {
+    public static void main(String[] args) {
+        
+        String str = "This is a string";
+        int len = str.length();
+        
+        System.out.println("String: " + str);
+        System.out.println("Length: " + len);
+    }
+    
+}
