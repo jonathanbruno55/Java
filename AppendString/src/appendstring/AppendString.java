23919678cf436baac41ab364d01c04f49e832d93
Jonathan Bruno <52010584+jonathanbruno55@users.noreply.github.com>
2021-03-09 03:40:24 -0600

    Add files via upload

---
+++ AppendString/src/appendstring/AppendString.java
@@ -0,0 +1,16 @@
+package appendstring;
+
+public class AppendString {
+    public static void main(String[] args) {
+        
+        String str1 = "I like jave";
+        String str2 = "I like pizza";
+        
+        System.out.println("str1: " + str1);
+        System.out.println("str2: " + str2);
+
+        String str3 = str1 + " and " + str2;
+        System.out.println("str3: " + str3);
+    }
+    
+}
