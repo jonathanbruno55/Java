dfc20157b582b84ba2d6ab883930b436d2572c86
Jonathan Bruno <52010584+jonathanbruno55@users.noreply.github.com>
2021-03-16 17:36:48 -0400

    Add files via upload

---
+++ UpperCase/src/uppercase/UpperCase.java
@@ -0,0 +1,15 @@
+package uppercase;
+
+public class UpperCase {
+
+    public static void main(String[] args) {
+        
+        String str = "abcdef ghijkl mnopqr stuvwxyz";
+        
+        String up = str.toUpperCase();
+        
+        System.out.println("Original string: " + str);
+        System.out.println("Upper case: " + up);
+    }
+    
+}
